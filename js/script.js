b99f794e0c2903062d6c167137b2f9d9f19c2c14
Eliana-Abate <eliana.abate@outlook.it>
2021-07-26 18:12:58 +0200

    edited ForEach cycle and printed icons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
 // Costruzione template icona con ciclo For-each
 
-let iconTemplate = '';
+const printIcons = (array, targetElement) => {
 
-icons.forEach ((icon) => {
-    //copio struttura creata in html e inserisco le proprietà degli oggetti
-    iconTemplate += `
-    <div class="col col-sm-4 col-md-2 offset-md-1">
-        <div class="card">
-            <div class="card-body">
-                <i class="${icons.family} ${icons.prefix}${icons.name} fa-2x"></i>
-                <h6>${icons.name}</h6>
+    let iconTemplate = '';
+
+    array.forEach((element) => {
+        //copio struttura creata in html, inserisco le proprietà degli oggetti
+        iconTemplate += `
+        <div class="col col-sm-4 col-md-2 offset-md-1">
+            <div class="card">
+                <div class="card-body">
+                    <i class="${element.family} ${element.prefix}${element.name} fa-2x"></i>
+                    <h6>${element.name.toUpperCase()}</h6>
+                </div>
             </div>
         </div>
-    </div>
-    `;
-});
+        `;
+    });
+
+    targetElement.innerHTML = iconTemplate;
+
+}
+ 
+
+
+
+// Stampa in Html
+const iconSection = document.querySelector('#icons .row');
+printIcons (icons, iconSection);
